b33ecc8fc7daac8d913122430f09ad8f39493348
fabiomagalhaes890 <fabiomagalhaes890@gmail.com>
2018-07-13 09:52:44 -0300

    events on form

--- src/textarea.js
+++ src/textarea.js
@@ -5,8 +5,25 @@ import React, { Component } from 'react'
 class TextArea extends Component {
   render () {
     return (
-      <form>
-        <textarea defaultValue={'padrao\nteste'} />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          console.log('event', e)
+        }}
+
+        onChange={(e) => {
+          console.log('name', e.target.name)
+          console.log('value', e.target.value)
+        }}
+      >
+        <input type='name' name='name' />
+        <input type='email' name='email' />
+        <textarea name='opa' defaultValue={'padrao\nteste'} />
+        <input type='checkbox'
+          onClick={(e) => {
+            alert('checked')
+          }} />
+        <button type='submit'>Enviar</button>
       </form>
     )
   }
